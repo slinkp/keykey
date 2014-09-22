2f36a1dce80690c47dd5929617e97f3809db37cf
Paul Winkler <slinkp@gmail.com>
2014-09-22 00:01:35 -0400

    FIx keykey to actually use window geometries including the frame. (Uses wmiface now to do this.)

--- keykey.py
+++ keykey.py
@@ -1,46 +1,87 @@
+#!/usr/bin/env python2
+
 """
-HACKS to replace slate.js with ewmh commands.
-
-TODO try  https://github.com/BurntSushi/xpybutil which is not pip-installable.  It provides an ewmh library.
-
-TODO is there any way to get geometries that INCLUDE the WM chrome??
- - Maybe wmiface (depends on Qt core).
-
-
- http://python-xlib.sourceforge.net/?page=documentation
- 
+HACKS to replace slate.js with wmiface / wmctrl / xdotool commands.
 """
 
 import subprocess
 import pprint
+import re
+
+LEFT = 'left'
+RIGHT = 'right'
+UP = 'up'
+DOWN = 'down'
+TOP = 'top'
+BOTTOM = 'bottom'
+
+geom_re = re.compile(r'(\d+)x(\d+)\+(-?\d+)\+(-?\d+)')
+
+
+def _as_hex(intstring):
+    return hex(int(intstring)).replace('0x', '0x0')
+
+
+def _as_intstring(hexstring):
+    hexstring = hexstring.split('x', 1)[-1]
+    return str(int(hexstring, 16))
 
 
 def get_window_list():
-    out = subprocess.check_output(['wmctrl', '-l', '-p', '-x', '-G'])
-    windows = [w.split(None, 9) for w in out.splitlines()]
-    windows = [{'id': w[0], 'desktop': w[1], 'pid': w[2],
-                'x': int(w[3]), 'y': int(w[4]),
-                'width': int(w[5]), 'height': int(w[6]),
-                'wm_class': w[7], 'title': w[8]
-                } for w in windows]
+    """
+    Get geometries of all windows on current workspace.
+    """
+    # XXX TODO: get geometry of each window INCLUDING its frame by doing
+    # wmiface frameGeometry id (integer id, not hex)
+    # ... returns eg. 650x437+0+872
+    # ... negative offset like 650x437+-78+905
+    # out = subprocess.check_output(['wmctrl', '-l', '-p', '-x', '-G'])
+    # windows = [w.split(None, 9) for w in out.splitlines()]
+    # windows = [{'id': w[0], 'desktop': w[1],
+    #             'pid': w[2],
+    #             'x': int(w[3]), 'y': int(w[4]),
+    #             'width': int(w[5]), 'height': int(w[6]),
+    #             'wm_class': w[7], 'title': w[8]
+    #             } for w in windows]
+
+    out = subprocess.check_output(['wmiface', 'normalWindows', '1'])
+    window_ids = out.splitlines()
 
     # Convenience borders.
-    for w in windows:
-        w['top'] = w['y']
-        w['right'] = w['x'] + w['width']
-        w['bottom'] = w['y'] + w['height']
-        w['left'] = w['x']
+    windows = []
+    for w_id in window_ids:
+        out = subprocess.check_output(['wmiface', 'frameGeometry', w_id])
+        # eg. 650x437+0+-31
+        width, height, x, y = map(int, geom_re.match(out).groups())
+        w = {'id': w_id,
+             'hex_id': _as_hex(w_id)}
+        windows.append(w)
+        w['width'] = width
+        w['height'] = height
+        w[LEFT] = w['x'] = x
+        w[TOP] = w['y'] = y
+        w[RIGHT] = w['x'] + w['width']
+        w[BOTTOM] = w['y'] + w['height']
 
     return windows
 
 
 def get_active_desktop_id():
+    # NOTE wmctrl counts from 0, wmiface counts from 1.
     out = subprocess.check_output(['wmctrl', '-d'])
     desktops = [d.split(None, 9) for d in out.splitlines()]
     for d in desktops:
         if d[1] == '*':
             return d[0]
     raise RuntimeError("Can't determine active desktop")
+
+
+def get_active_window_hex_id():
+    """
+    Get hex id of currently active window.
+    """
+    out = subprocess.check_output(['wmiface', 'activeWindow'])
+    return _as_hex(out.strip())
 
 
 def get_desktop_borders(desktop_id=None):
@@ -53,35 +94,51 @@ def get_desktop_borders(desktop_id=None):
     for d in desktops:
         desktop = {'id': d[0],
                    'active': True if d[1] == '*' else False,
-        }
+                   }
         # We use the "workspace" area, eg. not including the WM panel.
         workarea_x, workarea_y = map(int, d[7].split(','))
         workarea_w, workarea_h = map(int, d[8].split('x'))
-        desktop['top'] = workarea_y
-        desktop['left'] = workarea_x
-        desktop['right'] = workarea_x + workarea_w
-        desktop['bottom'] = workarea_y + workarea_h
+        desktop[TOP] = workarea_y
+        desktop[LEFT] = workarea_x
+        desktop[RIGHT] = workarea_x + workarea_w
+        desktop[BOTTOM] = workarea_y + workarea_h
         desktop_dicts.append(desktop)
 
         if desktop['id'] == desktop_id:
-            return (desktop['top'],
-                    desktop['right'],
-                    desktop['bottom'],
-                    desktop['left'],
+            return (desktop[TOP],
+                    desktop[RIGHT],
+                    desktop[BOTTOM],
+                    desktop[LEFT],
                     )
     raise ValueError("No desktop %d" % desktop_id)
 
 
-def get_window(window_id):
-    window = [w for w in get_window_list() if w['id'] == window_id][0]
-    return window
+def get_window_info_by_id(window_id):
+    """
+    Return geometry info for the specified window (int or hex).
+    """
+    windows = get_window_list()
+    windows = [w for w in windows if window_id in (w['id'], w['hex_id'])]
+    if len(windows) == 1:
+        return windows[0]
+    elif len(windows) > 1:
+        raise RuntimeError("Too many matching windows, should never happen. %s"
+                           % window_id)
+    else:
+        raise RuntimeError("Couldn't find window %s" % window_id)
 
 
-def get_all_window_borders(desktop_id=None):
+def get_all_window_borders(desktop_id=None, include_desktop=True,
+                           _windowlist=None):
     if desktop_id is None:
         desktop_id = get_active_desktop_id()
-    windows = get_window_list()
-    windows = [w for w in windows if w['desktop'] == desktop_id]
+
+    if _windowlist is None:
+        windows = get_window_list()
+    else:
+        windows = _windowlist
+
+    # windows = [w for w in windows if w['desktop'] == desktop_id]
     x_borders = set()
     y_borders = set()
     top, right, bottom, left = get_desktop_borders(desktop_id)
@@ -97,84 +154,92 @@ def get_all_window_borders(desktop_id=None):
             y_borders.add(y)
 
     for w in windows:
-        maybe_add_x(w['left'])
-        maybe_add_x(w['right'])
-        maybe_add_y(w['top'])
-        maybe_add_y(w['bottom'])
+        maybe_add_x(w[LEFT])
+        maybe_add_x(w[RIGHT])
+        maybe_add_y(w[TOP])
+        maybe_add_y(w[BOTTOM])
+
+    if include_desktop:
+        x_borders.add(left)
+        x_borders.add(right)
+        y_borders.add(top)
+        y_borders.add(bottom)
+
     x_borders = sorted(x_borders)
     y_borders = sorted(y_borders)
     return (x_borders, y_borders)
 
 
 def move_to_next_window_edge(window_id, direction):
-    win = get_window(window_id)
-    x_borders, y_borders = get_all_window_borders()
+    windows = get_window_list()
+
+    for win in windows:
+        if window_id in (win['id'], win['hex_id']):
+            print "Active window: %sx%s+%s+%s" % (win['width'], win['height'],
+                                                  win[LEFT], win[TOP])
+            break
+    else:
+        raise RuntimeError("Active window not found")
+
+    x_borders, y_borders = get_all_window_borders(
+        include_desktop=True,
+        _windowlist=windows)
     d_top, d_right, d_bottom, d_left = get_desktop_borders()
-    distances = []
-    if direction == 'right':
-        win_edge_1 = win['left']
-        win_edge_2 = win['right']
-        borders = x_borders
-        validate = lambda a, b: a > b and (a - b + win['right'] <= d_right)
-        reverse_distances = False
-    elif direction == 'left':
-        win_edge_1 = win['left']
-        win_edge_2 = win['right']
-        borders = reversed(x_borders)
-        validate = lambda a, b: a < b and (a - b + win['left'] >= d_left)
-        reverse_distances = True
-    elif direction == 'down':
-        win_edge_1 = win['top']
-        win_edge_2 = win['bottom']
-        borders = y_borders
-        validate = lambda a, b: a > b and (a - b + win['bottom'] <= d_bottom)
-        reverse_distances = False
-    elif direction == 'up':
-        win_edge_1 = win['top']
-        win_edge_2 = win['bottom']
-        borders = reversed(y_borders)
-        validate = lambda a, b: a < b and (a - b + win['top'] >= d_top)
-        reverse_distances = True
+
+    if direction in (RIGHT, LEFT):
+        win_edge_1 = win[LEFT]
+        span = win['width']
+        candidates = x_borders
+    elif direction in (UP, DOWN):
+        win_edge_1 = win[TOP]
+        span = win['height']
+        candidates = y_borders
     else:
-        raise ValueError("Direction was %r" % direction)
+        raise ValueError(u"Invalid direction %s" % direction)
 
-    for edge in borders:
-        if validate(edge, win_edge_1):
-            distances.append(edge - win_edge_1)
-        if validate(edge, win_edge_2):
-            distances.append(edge - win_edge_2)
+    if direction == RIGHT:
+        def is_valid(x):
+            return x > win_edge_1 and x <= (d_right - span)
+    elif direction == LEFT:
+        def is_valid(x):
+            return x < win_edge_1 and x >= d_left
+    elif direction == UP:
+        def is_valid(y):
+            return y < win_edge_1 and y >= d_top
+    elif direction == DOWN:
+        def is_valid(y):
+            return y > win_edge_1 and y <= (d_bottom - span)
 
-    MIN_DISTANCE = 12  # XXX hack for fvwm not including window chrome :(
-    distances = [d for d in distances if abs(d) >= MIN_DISTANCE]
-    distances.sort(reverse=reverse_distances)
-    print "Distances: "
-    pprint.pprint(distances)
-    if not distances:
-        print "NOwhere to move"
-        return
+    candidates.extend([c - span for c in candidates])
+    candidates = sorted(set(candidates))
+    candidates = [c for c in candidates if is_valid(c)]
 
-    def move(x, y):
-        print "WOULD MOVE TO: %d, %d" % (x, y)
+    print "Candidates are: ", candidates
 
-    if direction in ("left", "right"):
-        move_window_to(window_id, x=win['left'] + distances[0])
-    else:
-        move_window_to(window_id, y=win['top'] + distances[0])
+    if candidates:
+        if direction == RIGHT:
+            move_window_to(window_id, candidates[0], win[TOP])
+        if direction == LEFT:
+            move_window_to(window_id, candidates[-1], win[TOP])
+        if direction == UP:
+            move_window_to(window_id, win[LEFT], candidates[-1])
+        if direction == DOWN:
+            move_window_to(window_id, win[LEFT], candidates[0])
 
 
 def move_window_to(window_id, x=-1, y=-1):
-    # mvarg = '0,%d,%d,-1,-1' % (x, y)
-    mvarg = '1,%d,%d,-1,-1' % (x, y)   # experiment w/ gravity
+    mvarg = '0,%d,%d,-1,-1' % (x, y)
+    # mvarg = '1,%d,%d,-1,-1' % (x, y)   # experiment w/ gravity
+    # XXX TODO use `wmiface frameGeometry id x y` where id is int
     cmd = ['wmctrl', '-i', '-r', window_id, '-e', mvarg]
     out = subprocess.check_output(cmd)
     print ' '.join(cmd), "...with output:", out
 
 if __name__ == '__main__':
-    pprint.pprint(get_desktop_borders())
-    pprint.pprint(get_all_window_borders())
-    WIN_ID = '0x02c00177'
-
+    # pprint.pprint(get_desktop_borders())
+    # pprint.pprint(get_all_window_borders())
+    win_id = get_active_window_hex_id()
     import sys
     direction = sys.argv[1]
     print "==== %s ============" % direction
-    move_to_next_window_edge(WIN_ID, direction)
+    move_to_next_window_edge(win_id, direction)
