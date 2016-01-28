53872a3a1cb9863159be5e4a1d826563803be769
Paul Winkler <slinkp@gmail.com>
2016-01-27 22:13:08 -0500

    Allow snapping to desktop center. And lotsa TODO comment updates

--- keykey.py
+++ keykey.py
@@ -2,10 +2,14 @@
 
 """
 HACKS to replace slate.js with wmiface / wmctrl / xdotool commands.
+
+TODO replace wmiface with combination of xdotool, xwinfo, xprop...
+eg. see second answer part of
+http://unix.stackexchange.com/a/156349/84607
+which can be  simplified (easier to parse in python than bash+sed)
 """
 
 import subprocess
-import pprint
 import re
 
 LEFT = 'left'
@@ -31,19 +35,6 @@ def get_window_list():
     """
     Get geometries of all windows on current workspace.
     """
-    # XXX TODO: get geometry of each window INCLUDING its frame by doing
-    # wmiface frameGeometry id (integer id, not hex)
-    # ... returns eg. 650x437+0+872
-    # ... negative offset like 650x437+-78+905
-    # out = subprocess.check_output(['wmctrl', '-l', '-p', '-x', '-G'])
-    # windows = [w.split(None, 9) for w in out.splitlines()]
-    # windows = [{'id': w[0], 'desktop': w[1],
-    #             'pid': w[2],
-    #             'x': int(w[3]), 'y': int(w[4]),
-    #             'width': int(w[5]), 'height': int(w[6]),
-    #             'wm_class': w[7], 'title': w[8]
-    #             } for w in windows]
-
     out = subprocess.check_output(['wmiface', 'normalWindows', '1'])
     window_ids = out.splitlines()
 
@@ -67,7 +58,7 @@ def get_window_list():
 
 
 def get_active_desktop_id():
-    # NOTE wmctrl counts from 0, wmiface counts from 1.
+    # NOTE wmctrl and xdotool count from 0, wmiface counts from 1.
     out = subprocess.check_output(['wmctrl', '-d'])
     desktops = [d.split(None, 9) for d in out.splitlines()]
     for d in desktops:
@@ -129,6 +120,7 @@ def get_window_info_by_id(window_id):
 
 
 def get_all_window_borders(desktop_id=None, include_desktop=True,
+                           include_center=True,
                            _windowlist=None):
     if desktop_id is None:
         desktop_id = get_active_desktop_id()
@@ -163,6 +155,11 @@ def get_all_window_borders(desktop_id=None, include_desktop=True,
         x_borders.add(right)
         y_borders.add(top)
         y_borders.add(bottom)
+        if include_center:
+            center_x = (right - left) // 2
+            maybe_add_x(center_x)
+            center_y = (bottom - top) // 2
+            maybe_add_y(center_y)
 
     x_borders = sorted(x_borders)
     y_borders = sorted(y_borders)
@@ -183,18 +180,27 @@ def move_to_next_window_edge(window_id, direction):
         raise RuntimeError("Active window not found")
 
     x_borders, y_borders = get_all_window_borders(
-        include_desktop=True,
+        include_desktop=True, include_center=True,
         _windowlist=windows)
+
     d_top, d_right, d_bottom, d_left = get_desktop_borders()
+
+    # Centering the window has to be done in this func. because it depends
+    # on dimensions of THIS window, which get_all_window_borders
+    # doesn't know.
+    d_center_x = (d_right - d_left) // 2
+    d_center_y = (d_bottom - d_top) // 2
 
     if direction in (RIGHT, LEFT):
         win_edge_1 = win[LEFT]
         span = win['width']
         candidates = x_borders
+        edge_to_center_1 = d_center_x - (span // 2)
     elif direction in (UP, DOWN):
         win_edge_1 = win[TOP]
         span = win['height']
         candidates = y_borders
+        edge_to_center_1 = d_center_y - (span // 2)
     else:
         raise ValueError(u"Invalid direction %s" % direction)
 
@@ -211,7 +217,13 @@ def move_to_next_window_edge(window_id, direction):
         def is_valid(y):
             return y > win_edge_1 and y <= (d_bottom - span)
 
+    # Allow snapping to both sides of an edge.
     candidates.extend([c - span for c in candidates])
+    # Allow snapping to exact screen center.
+    # This is after the previous line on purpose -
+    # don't want to snap to center + 1/2 span for no reason.
+    candidates.append(edge_to_center_1)
+
     candidates = sorted(set(candidates))
     candidates = [c for c in candidates if is_valid(c)]
 
@@ -235,6 +247,11 @@ def move_window_to(window_id, x=-1, y=-1):
     cmd = ['wmctrl', '-i', '-r', window_id, '-e', mvarg]
     out = subprocess.check_output(cmd)
     print ' '.join(cmd), "...with output:", out
+
+
+# TODO another command to maximize to next edge?
+
+# TODO another command to emulate 'focus right' et al. from slate
 
 if __name__ == '__main__':
     # pprint.pprint(get_desktop_borders())
